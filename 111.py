a56042841688fbeb84b4eadf3550e8224d291851
Evgeny905 <gorbatkovei@gmail.com>
2024-06-20 08:27:16 -0700

    Add files via upload

---
+++ 111.py
@@ -0,0 +1,50 @@
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index_spisok = 0
+len(my_list)
+
+
+
+
+
+
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+my_list_1 = 0
+my_list_13 = len(my_list)
+
+while True: and my_list_1 + 1 <= my_list_13
+    if my_list[my_list_1] > 0:
+        print(my_list[my_list_1])
+    if my_list[my_list_1 + 1] > 0 and my_list_1 + 1 <= my_list_13:
+        print(my_list[my_list_1 + 1])
+    if my_list[my_list_1 + 2] > 0:
+        print(my_list[my_list_1 + 2])
+    if my_list[my_list_1 + 3] > 0:
+        print(my_list[my_list_1 + 3])
+    if my_list[my_list_1 + 4] > 0:
+        print(my_list[my_list_1 + 4])
+    if my_list[my_list_1+5] > 0:
+        print(my_list[my_list_1+5])
+    if my_list[my_list_1 + 6] > 0:
+        print(my_list[my_list_1 + 6])
+    if my_list[my_list_1 + 7] > 0:
+        print(my_list[my_list_1 + 7])
+    if my_list[my_list_1 + 8] > 0:
+        print(my_list[my_list_1 + 8])
+    if my_list[my_list_1 + 9] > 0:
+        print(my_list[my_list_1 + 9])
+    if my_list[my_list_1 + 10] > 0:
+        print(my_list[my_list_1 + 10])
+    if my_list[my_list_1 + 11] > 0:
+        print(my_list[my_list_1 + 11])
+    if my_list[my_list_1 + 12] > 0:
+        print(my_list[my_list_1 + 12])
+    if my_list[my_list_1 + 13] > 0:
+        print(my_list[my_list_1 + 13])
+
+
+        break
+
+
+
+
+
