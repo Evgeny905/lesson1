1c8ddbebfd3028695d20c28fe04b5687eb0d7d0d
Evgeny905 <gorbatkovei@gmail.com>
2024-06-22 19:22:04 +0700

    Update module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -1,8 +1,8 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
-primes = []
+not_primes = []
 
-not_primes = numbers
+primes = numbers
 
 is_prime = False
 
@@ -11,9 +11,9 @@ for i in numbers:
         if i % j == 0:
             is_prime = True
             if is_prime:
-                primes.append(i)
-            else:
                 not_primes.append(i)
+            else:
+                primes.append(i)
             break
 
 for i in numbers:
@@ -21,7 +21,7 @@ for i in numbers:
         if i % j == 0:
             is_prime = True
             if is_prime:
-                not_primes.remove(i)
+                primes.remove(i)
             break
 
 for k in numbers:
@@ -29,7 +29,7 @@ for k in numbers:
         if k % l == 0:
             is_prime = True
             if is_prime:
-                not_primes.remove(k)
+                primes.remove(k)
             break
 
 print('Primes:', primes)
